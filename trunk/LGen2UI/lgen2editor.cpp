a13e70ece23446444e19d8effb99abf2015a576b
antonstorozhev@gmail.com <antonstorozhev@gmail.com@0619d246-e3bf-110d-73bc-cc41b15f2e0f>
2012-05-08 07:52:08 +0000

    Форма редактирование онтологии: начал делать риббон

--- trunk/LGen2UI/lgen2editor.cpp
+++ trunk/LGen2UI/lgen2editor.cpp
@@ -6,23 +6,6 @@ LGen2Editor::LGen2Editor(QWidget *parent) :
     ui(new Ui::LGen2Editor)
 {
     ui->setupUi(this);
-
-    ui->splitter->setSizes(QList<int>() << 1 << 7 << 1);
-    ui->splitter_2->setSizes(QList<int>() << 1 << 7 << 1);
-
-    //Запихнуть всё создание начальной БЗ в XMLину
-    //TODO сделать нормальную генерацию айдишников
-    ui->domainDiagram->addNode(0, "#Thing");
-    ui->domainDiagram->addNode(1, "#Ситуация");
-    ui->domainDiagram->addNode(2, "#Сущность");
-    ui->domainDiagram->addNode(3, "#Действие");
-    ui->domainDiagram->addLink(1, 0, "is-a");
-    ui->domainDiagram->addLink(2, 0, "is-a");
-    ui->domainDiagram->addLink(3, 0, "is-a");
-
-    ui->templateDiagram->addNode(0, "#Thing");
-    ui->templateDiagram->addNode(1, "#Шаблон");
-    ui->templateDiagram->addLink(1, 0, "is-a");
 }
 
 LGen2Editor::~LGen2Editor()
